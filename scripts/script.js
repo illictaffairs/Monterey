afaca53fee523a81d3c22858461b7e72b7110cf6
illictaffairs <114866139+illictaffairs@users.noreply.github.com>
2022-10-17 19:48:18 +0300

    uploaded

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+alert("only for educational purposes, all fonts and symbols copyright to apple, developed by me, github: illictaffairs");
+function dropApple() {
+	document.getElementById("AppleDrop").classList.toggle("show");
+}
+var time = new Date();
+console.log(time);
+document.getElementById("time").textContent = time; //it will print on html page
